e7af2d85935af5bfcbe62d74a620c1ce5a23213c
nipunsa <nipun@corali.tech>
2025-01-12 18:45:30 +0530

    populate product wise and add product to cart

--- src/app/[channel]/layout.tsx
+++ src/app/[channel]/layout.tsx
@@ -1,9 +1,33 @@
 import { type ReactNode } from "react";
 import { Header } from "@/ui/components/Header";
+import { executeGraphQL } from "@/lib/graphql";
+import { ChannelsListDocument } from "@/gql/graphql";
 
 export const metadata = {
   title: "playa3ull ecommerce platform",
   description: "building performant e-commerce experiences with Saleor.",
+};
+
+export const generateStaticParams = async () => {
+  // the `channels` query is protected
+  // you can either hardcode the channels or use an app token to fetch the channel list here
+
+  if (process.env.SALEOR_APP_TOKEN) {
+    const channels = await executeGraphQL(ChannelsListDocument, {
+      withAuth: false, // disable cookie-based auth for this call
+      headers: {
+        // and use app token instead
+        Authorization: `Bearer ${process.env.SALEOR_APP_TOKEN}`,
+      },
+    });
+    return (
+      channels.channels
+        ?.filter((channel) => channel.isActive)
+        .map((channel) => ({ channel: channel.slug })) ?? []
+    );
+  } else {
+    return [{ channel: "default-channel" }];
+  }
 };
 
 export default async function RootLayout(props: {
