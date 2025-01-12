75eae37bb05bc196f1ad164bd352a12620232441
nipunsa <nipun@corali.tech>
2025-01-12 18:49:53 +0530

    add checkout page not implemented

---
+++ src/app/[channel]/checkout/page.tsx
@@ -0,0 +1,23 @@
+export const metadata = {
+  title: "Checkout",
+};
+
+export default function CheckoutPage({
+  searchParams,
+}: {
+  searchParams: { checkout?: string; order?: string };
+}) {
+  if (!searchParams.checkout && !searchParams.order) {
+    return null;
+  }
+
+  return (
+    <div className="min-h-dvh bg-white">
+      <section className="mx-auto flex min-h-dvh max-w-7xl flex-col p-8">
+        <h1 className="mt-8 text-3xl font-bold text-neutral-900">
+          Checkout to be implemented
+        </h1>
+      </section>
+    </div>
+  );
+}
